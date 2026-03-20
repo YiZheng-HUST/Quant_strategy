9fc9d9ee89ef5548b4065d8b7f54ca0d5ca57573
zhengyi <ricozheng@asrmicro.com>
2026-03-20 14:46:32 +0800

    multiple source for us etf

--- finance_analysis.py
+++ finance_analysis.py
@@ -5,44 +5,48 @@ import matplotlib.pyplot as plt
 from currency_converter import CurrencyConverter
 from datetime import datetime, timedelta
 
+import logging
+
 # ================= Dynamic Config Area =================
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 today = datetime.now()
 one_year_ago = today - timedelta(days=365)
 
 START_DATE = one_year_ago.strftime("%Y%m%d")
 END_DATE = today.strftime("%Y%m%d")
-
 DATA_DIR = "raw_data/"
 
 # Define lists for Chinese ETFs
-A_SHARE_ETFS = ["513300", # 纳斯达克ETF
-                "513500", # 标普500ETF
-                "518880", # 华安黄金ETF
-                "515450", # 红利低波50ETF
-                "513360", # 短融ETF海富通
-                "510880", # 红利ETF华泰柏瑞
-                "512480", # 半导体ETF
-                ]
+A_SHARE_ETFS = {
+    "nasdaq_etf": "513300",  # 纳斯达克ETF
+    "sp500_etf": "513500",  # 标普500ETF
+    "huaan_gold_etf": "518880",  # 华安黄金ETF
+    "red_low_volatility_50_etf": "515450",  # 红利低波50ETF
+    "short_term_bond_etf": "513360",  # 短融ETF海富通
+    "red_etf_huatai": "510880",  # 红利ETF华泰柏瑞
+    "semiconductor_etf": "512480",  # 半导体ETF
+}
 
 # Define lists for American ETFs east money
-US_SHARE_ETFS_EASTMONEY = [ "105.QQQ", # 纳斯达克
-                            "106.SPY", # 标普500
-                            "106.DIA", # 道琼斯
-                            "106.GLD", # 黄金
-                            "105.SHY", # 短期美债
-                            "105.IEF", # 中期美债
-                            "105.TLT", # 长期美债
-                            ]
+US_SHARE_ETFS_EASTMONEY = {
+    "nasdaq": "105.QQQ",
+    "sp500": "106.SPY",
+    "dow_jones": "106.DIA",
+    "gold": "106.GLD",
+    "short_term_treasury": "105.SHY",
+    "mid_term_treasury": "105.IEF",
+    "long_term_treasury": "105.TLT",
+}
 
-# Define lists for American ETFs sina
-US_SHARE_ETFS_SINA = [  "QQQ", # 纳斯达克
-                        "SPY", # 标普500
-                        "DIA", # 道琼斯
-                        "GLD", # 黄金
-                        "SHY", # 短期美债
-                        "IEF", # 中期美债
-                        "TLT", # 长期美债
-                        ]
+US_SHARE_ETFS_SINA = {
+    "nasdaq": "QQQ",
+    "sp500": "SPY",
+    "dow_jones": "DIA",
+    "gold": "GLD",
+    "short_term_treasury": "SHY",
+    "mid_term_treasury": "IEF",
+    "long_term_treasury": "TLT"}
 
 # Define data source
 DATA_SOURCE = ["eastmoney", # 东方财富
@@ -50,23 +54,27 @@ DATA_SOURCE = ["eastmoney", # 东方财富
                "tencent"]   # 腾讯
 # =======================================================
 
-def fetch_and_save_data(a_etf_list, us_etf_list, data_source: str, start_date, end_date):
+def fetch_and_save_data(a_etf_dict, us_etf_dict_eastmoney, us_etf_dict_sina, data_source: str, start_date, end_date):
     dir_path = os.path.join(DATA_DIR, END_DATE)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
         print(f"Created directory: {dir_path}")
+        logging.info(f"Created directory: {dir_path}")
 
     a_data = {}
     us_data = {}
 
     # Fetch A-Share ETFs
-    for symbol in a_etf_list:
+    for name, symbol in a_etf_dict.items():
+        print(f"name = {name}, symbol = {symbol}")
         file_path = os.path.join(dir_path, f"{symbol}_{end_date}.csv")
         if os.path.exists(file_path):
-            print(f"Found local cache for {symbol}. Loading...")
+            print(f"Found local cache for A-share ETF {symbol}. Loading...")
+            logging.info(f"Found local cache for A-share ETF {symbol}. Loading...")
             a_data[symbol] = pd.read_csv(file_path)
         else:
             print(f"Fetching data for A-share ETF {symbol} ({start_date} - {end_date})...")
+            logging.info(f"Fetching data for A-share ETF {symbol} from {data_source} ({start_date} - {end_date})...")
             if data_source == "eastmoney":
                 df = ak.fund_etf_hist_em(symbol=symbol, period="daily", start_date=start_date, end_date=end_date, adjust="qfq") # 东方财富接口
             elif data_source == "sina":
@@ -75,27 +83,37 @@ def fetch_and_save_data(a_etf_list, us_etf_list, data_source: str, start_date, e
                 df = ak.stock_zh_a_hist(symbol="sh" + symbol, start_date=start_date, end_date=end_date, adjust="qfq") # 腾讯接口，沪市为例
             else:
                 raise ValueError("Invalid data source.")
-
             df.to_csv(file_path, index=False, encoding="utf-8-sig")
             a_data[symbol] = df
             print(f"Saved {symbol} data to {file_path}")
-
+            logging.info(f"Saved A-share ETF {symbol} data to {file_path}")
+    
     # Fetch US-Share ETFs
-    for symbol in us_etf_list:
+    for name, symbol in us_etf_dict_eastmoney.items():
         file_path = os.path.join(dir_path, f"{symbol}_{end_date}.csv")
         if os.path.exists(file_path):
-            print(f"Found local cache for {symbol}. Loading...")
+            print(f"Found local cache for US ETF {symbol}. Loading...")
+            logging.info(f"Found local cache for US ETF {symbol}. Loading...")
             us_data[symbol] = pd.read_csv(file_path)
         else:
-            print(f"Fetching data for US ETF {symbol} ({start_date} - {end_date})...")
-            if data_source == "eastmoney":
+            try:
+                print(f"Fetching data for US ETF {symbol} from eastmoney ({start_date} - {end_date})...")
+                logging.info(f"Fetching data for US ETF {symbol} from eastmoney ({start_date} - {end_date})...")
                 df = ak.stock_us_hist(symbol=symbol, period="daily", start_date=start_date, end_date=end_date, adjust="qfq")
-            elif data_source == "sina":
-                df = ak.stock_us_daily(symbol=symbol, adjust="qfq") # 新浪接口，沪市为例
-            df.to_csv(file_path, index=False, encoding="utf-8-sig")
-            us_data[symbol] = df
-            print(f"Saved {symbol} data to {file_path}")
-    
+                df.to_csv(file_path, index=False, encoding="utf-8-sig")
+                us_data[symbol] = df
+                print(f"Saved {symbol} data to {file_path}")
+                logging.info(f"Saved US ETF {symbol} data to {file_path}")
+            except Exception as e:
+                print(f"Failed to fetch data from eastmoney for {symbol}: {e}. Trying sina...")
+                logging.warning(f"Failed to fetch data from eastmoney for {symbol}: {e}. Trying sina...")
+                symbol = us_etf_dict_sina[name]
+                print(f"Fetching data for US ETF {symbol} from sina ({start_date} - {end_date})...")
+                logging.info(f"Fetching data for US ETF {symbol} from sina ({start_date} - {end_date})...")
+                df = ak.stock_us_daily(symbol=symbol, adjust="qfq")
+                df.to_csv(file_path, index=False, encoding="utf-8-sig")
+                us_data[symbol] = df
+                print(f"Saved {symbol} data to {file_path}")
     return a_data, us_data
 
 def process_and_plot(a_data_dict, us_data_dict, a_target, us_target):
@@ -169,8 +187,7 @@ def process_and_plot(a_data_dict, us_data_dict, a_target, us_target):
 
 if __name__ == "__main__":
     # Fetch all data from the lists
-    dict_a, dict_u = fetch_and_save_data(A_SHARE_ETFS, US_SHARE_ETFS_SINA, DATA_SOURCE[1], START_DATE, END_DATE)
-    
+    dict_a, dict_u = fetch_and_save_data(A_SHARE_ETFS, US_SHARE_ETFS_EASTMONEY, US_SHARE_ETFS_SINA, DATA_SOURCE[1], START_DATE, END_DATE)
 
     # Plot the first item in each list by default
-    process_and_plot(dict_a, dict_u, A_SHARE_ETFS[0], US_SHARE_ETFS_SINA[0])
+    # process_and_plot(dict_a, dict_u, A_SHARE_ETFS[0], US_SHARE_ETFS_SINA[0])
