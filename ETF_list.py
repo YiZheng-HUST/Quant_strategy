fa314c0a2593a03b0cbbf4bece919677c966c1a9
zhengyi <ricozheng@asrmicro.com>
2026-03-24 17:44:55 +0800

    update us etf fee

--- ETF_list.py
+++ ETF_list.py
@@ -10,26 +10,27 @@ A_SHARE_ETFS = {
     "A_red_etf_huatai": {"symbol": "510880", "management_fee": 0.005, "trustee_fee": 0.001},  # 红利ETF华泰柏瑞
     "A_short_term_bond_etf": {"symbol": "511360", "management_fee": 0.0015, "trustee_fee": 0.0005},  # 短融ETF海富通
     "A_huaan_gold_etf": {"symbol": "518880", "management_fee": 0.005, "trustee_fee": 0.001},  # 华安黄金ETF
+    "A_hushen_300_huatai_etf": {"symbol": "510300", "management_fee": 0.0015, "trustee_fee": 0.0005},  # 华泰柏瑞沪深300ETF
 }
 
 # Define lists for American ETFs in eastmoney
 US_SHARE_ETFS_EASTMONEY = {
-    "nasdaq": {"symbol": "105.QQQ", "management_fee": 0, "trustee_fee": 0},
-    "sp500": {"symbol": "106.SPY", "management_fee": 0, "trustee_fee": 0},
-    "dow_jones": {"symbol": "106.DIA", "management_fee": 0, "trustee_fee": 0},
-    "gold": {"symbol": "106.GLD", "management_fee": 0, "trustee_fee": 0},
-    "short_term_treasury": {"symbol": "105.SHY", "management_fee": 0, "trustee_fee": 0},
-    "mid_term_treasury": {"symbol": "105.IEF", "management_fee": 0, "trustee_fee": 0},
-    "long_term_treasury": {"symbol": "105.TLT", "management_fee": 0, "trustee_fee": 0},
+    "nasdaq": {"symbol": "105.QQQ", "total_fee": 0.002},
+    "sp500": {"symbol": "106.SPY", "total_fee": 0.0009},
+    "dow_jones": {"symbol": "106.DIA", "total_fee": 0},
+    "gold": {"symbol": "106.GLD", "total_fee": 0.004},
+    "short_term_treasury": {"symbol": "105.SHY", "total_fee": 0},
+    "mid_term_treasury": {"symbol": "105.IEF", "total_fee": 0},
+    "long_term_treasury": {"symbol": "105.TLT", "total_fee": 0.0015},
 }
 
 # Define lists for American ETFs in sina
 US_SHARE_ETFS_SINA = {
-    "nasdaq": {"symbol": "QQQ", "management_fee": 0, "trustee_fee": 0},
-    "sp500": {"symbol": "SPY", "management_fee": 0, "trustee_fee": 0},
-    "dow_jones": {"symbol": "DIA", "management_fee": 0, "trustee_fee": 0},
-    "gold": {"symbol": "GLD", "management_fee": 0, "trustee_fee": 0},
-    "short_term_treasury": {"symbol": "SHY", "management_fee": 0, "trustee_fee": 0},
-    "mid_term_treasury": {"symbol": "IEF", "management_fee": 0, "trustee_fee": 0},
-    "long_term_treasury": {"symbol": "TLT", "management_fee": 0, "trustee_fee": 0},
+    "nasdaq": {"symbol": "QQQ", "total_fee": 0.002},
+    "sp500": {"symbol": "SPY", "total_fee": 0.0009},
+    "dow_jones": {"symbol": "DIA", "total_fee": 0},
+    "gold": {"symbol": "GLD", "total_fee": 0.004},
+    "short_term_treasury": {"symbol": "SHY", "total_fee": 0},
+    "mid_term_treasury": {"symbol": "IEF", "total_fee": 0},
+    "long_term_treasury": {"symbol": "TLT", "total_fee": 0.0015},
 }
