bd516714586af9e53281e4552b038895c10084c9
zhengyi <ricozheng@asrmicro.com>
2026-03-24 18:44:07 +0800

    update ETF list

--- ETF_list.py
+++ ETF_list.py
@@ -4,13 +4,15 @@
 
 # Define lists for Chinese ETFs
 A_SHARE_ETFS = {
-    "A_nasdaq_etf": {"symbol": "513300", "management_fee": 0.006, "trustee_fee": 0.002},  # 纳斯达克ETF
-    "A_sp500_etf": {"symbol": "513500", "management_fee": 0.006, "trustee_fee": 0.002},  # 标普500ETF
-    "A_red_low_volatility_50_etf": {"symbol": "515450", "management_fee": 0.005, "trustee_fee": 0.001},  # 红利低波50ETF
+    "A_nasdaq_etf": {"symbol": "513300", "management_fee": 0.006, "trustee_fee": 0.002},  # 华夏纳斯达克ETF
+    "A_sp500_etf": {"symbol": "513500", "management_fee": 0.006, "trustee_fee": 0.002},  # 博时标普500ETF
+    "A_red_low_volatility_50_etf": {"symbol": "515450", "management_fee": 0.005, "trustee_fee": 0.001},  # 南方红利低波50ETF
     "A_red_etf_huatai": {"symbol": "510880", "management_fee": 0.005, "trustee_fee": 0.001},  # 红利ETF华泰柏瑞
     "A_short_term_bond_etf": {"symbol": "511360", "management_fee": 0.0015, "trustee_fee": 0.0005},  # 短融ETF海富通
     "A_huaan_gold_etf": {"symbol": "518880", "management_fee": 0.005, "trustee_fee": 0.001},  # 华安黄金ETF
     "A_hushen_300_huatai_etf": {"symbol": "510300", "management_fee": 0.0015, "trustee_fee": 0.0005},  # 华泰柏瑞沪深300ETF
+    "A_fullgoal_hk_internet_etf": {"symbol": "159792", "management_fee": 0.005, "trustee_fee": 0.001}, # 富国中证港股通互联网ETF
+    "A_chinaamc_hk_tech_etf": {"symbol": "513180", "management_fee": 0.005, "trustee_fee": 0.0015}, # 华夏恒生科技ETF
 }
 
 # Define lists for American ETFs in eastmoney
